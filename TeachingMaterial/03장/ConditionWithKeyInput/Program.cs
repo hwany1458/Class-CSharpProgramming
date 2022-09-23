fb977137f58f22481c151675bdc0e5d99832da06
hwany1458 <hwany1458@empal.com>
2022-09-23 18:47:53 +0900

    2장 3장 업로드

---
+++ TeachingMaterial/03장/ConditionWithKeyInput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionWithKeyInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo info = Console.ReadKey();
+            switch (info.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("위로 이동");
+                    break;
+                case ConsoleKey.RightArrow:
+                    Console.WriteLine("오른쪽으로 이동");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("아래로 이동");
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Console.WriteLine("왼쪽으로 이동");
+                    break;
+                default:
+                    Console.WriteLine("다른 키를 눌렀습니다");
+                    break;
+            }
+        }
+    }
+}
