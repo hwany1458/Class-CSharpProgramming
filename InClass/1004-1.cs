89a901185e76309b6c6be8c96a14a8f1c4ba2a51
hwany1458 <hwany1458@empal.com>
2022-10-05 02:50:28 +0900

    10.04. commit

---
+++ InClass/1004-1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while 반복문
+            int i = 1;
+            
+            //while(true)  // 무한반복
+            while (i <= 10)
+            {
+                Console.WriteLine("이용환 " + i);
+                i++;
+            }
+            //--------------------
+
+            // 키보드로부터 정수를 입력받아, 출력하기
+            // 정수가 0이 아닐 때, 계속해서 입력을 받고
+            // (만약 입력받은) 정수가 0이면 반복을 마치고 끝냄
+
+            string inputStr;
+            int inputNum;
+
+            /*
+            Console.Write("가위(1), 바위(2), 보(3), 종료(0)을 입력 ");
+            inputStr = Console.ReadLine();
+            inputNum = int.Parse(inputStr);
+            */
+
+            // while() {..} 조건을 체크해서 참일 때 실행
+            // (만약) 조건이 처음부터 거짓이면 반복문을 실행하지 않음
+            // do {..} while(); 먼저 한번을 실행한 다음, 조건을 체크
+
+            //while(inputNum != 0)
+            // while --> do while 으로 변경
+            do
+            {
+                Console.Write("가위(1), 바위(2), 보(3), 종료(0)을 입력 ");
+                inputStr = Console.ReadLine();
+                inputNum = int.Parse(inputStr);
+
+                Console.WriteLine($"입력받는 수는 {inputNum}");
+            } while (inputNum != 0);
+
+
+            // 숙제
+            // 키보드로부터 화살표(또는 ASWD) 키를 입력받아서, 앞뒤좌우을 표시
+            // 여기까지는 9월27일 코드 참조
+            // 수정 -- 키보드로부터 ESC키를 입력받을 때까지 
+            //         반복적으로 위의 내용을 수행
+            // do while()문으로 작성합니다.
+
+            //------------------------
+            // 1~10까지 반복해서 출력해보세요
+            // 수정 : 홀수만 출력하고 싶어요
+            // (1) 2씩 증가하게 -- 시작이 짝수면 짝수가 출력 (좋은 방법 아님)
+            for (int x = 1; x <= 10; x++)
+            {
+                // (2) 홀수인지를 체크해서 홀수인 경우만 출력
+                if ((x % 2) == 0)
+                {
+                    // continue 일때와 break 일때의 차이를 확인하세요
+                    continue;
+                    //break;
+                }
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ InClass/1004.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 배열 (선언, 값 입력)  []
+            int[] arr = { 10, 20, 30, 40 };  // arr 이라는 이름으로 정수형 배열을 선언
+            // in C, int arr[];
+            Console.WriteLine(arr[1]);
+            arr[2] = 100;
+
+            for (int i = 0; i < arr.Length; i++)  // 인덱스번호 : 0, 1, 2, 3
+            {
+                Console.WriteLine(i + "번째 데이타는 " + arr[i]);
+            }
+            
+            // 여기까지는 지난시간에 했던 내용
+            //-----------------
+            
+            //foreach (int ele in arr)
+            foreach (var ele in arr)
+            {
+                Console.WriteLine("가지고온 데이타는 " + ele);
+            }
+
+            string[] arrStr = { "없습니다", "스테이크", "초밥", "돈까스", "삼겹살소주", "소고기(한우++)" };
+            //foreach(string s in arrStr)
+            // foreach()에서 자료형은 데이터가 들어가는 시점에서 결정하는 var로 선언하도록 
+            foreach (var s in arrStr)   // --- C#스럽게
+            {
+                Console.WriteLine(s);
+            }
+
+            // for문  (동작은 똑같음)  --- C스럽게
+            for(int i=0; i<arrStr.Length; i++)
+            {
+                Console.WriteLine(arrStr[i]);
+            }
+        }
+    }
+}
