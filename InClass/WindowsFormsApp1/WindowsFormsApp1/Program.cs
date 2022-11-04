66eea3c3ab3e574ef68c8b97638d8e0d5e6096ca
hwany1458 <hwany1458@empal.com>
2022-11-04 09:29:22 +0900

    upload 11.03.

---
+++ InClass/WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        ///  프로그램 시작위치
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Form1 클래스에서 객체 생성해서 실행
+            Application.Run(new Form1());
+        }
+    }
+}
