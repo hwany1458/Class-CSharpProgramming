f8431496d945cea2d86770ee045c218763582244
hwany1458 <hwany1458@empal.com>
2023-09-07 23:52:29 +0900

    0907

---
+++ InClass2023/FirstProject_Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 여기에 코드 작성
+
+            // C에서 printf("Hello world\n"); 와 동일
+            Console.WriteLine("명령이 끝나면 세미콜론(;)을 찍어줍니다");
+            Console.WriteLine("대소문자를 구분");
+            Console.WriteLine("주석처리는 C언어와 동일하게 //, /* */를 사용");
+
+            Console.Write("이용환");
+            Console.Write("이현순");
+        }
+    }
+}
