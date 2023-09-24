122d0dd6ba896a28b6b144bdb67d1e1614700595
hwany1458 <hwany1458@empal.com>
2023-09-24 13:44:56 +0900

    Add files via upload

---
+++ InClass2023/0921.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (1)변수선언 <-- 정수 입력(키보드로부터)
+            // (2) if문으로 홀수인지를 체크 (%연산 == 1)
+            //    참이면 출력
+            
+            // (1)
+            int inNum;  // inNum이라는 이름으로 정수형 변수룰 선언
+            string inStr = Console.ReadLine();
+            inNum = int.Parse(inStr);
+
+            // (2)
+            if ((inNum%2) == 1)
+            {
+                Console.WriteLine("입력한 " + inNum + "은 홀수입니다");
+            }
+            else
+            {
+                Console.WriteLine("입력한 " + inNum + "은 짝수");
+            }
+
+            // 입력받은 정수가 양수인지, 음수인지, 0인지를 조건문으로 체크해서 출력하세요
+            if (inNum > 0) { Console.WriteLine("양수입니다"); }
+            else {
+                if (inNum < 0) { Console.WriteLine("음수"); }
+                else { Console.WriteLine("입력받은 정수는 0입니다");  }
+            }
+
+            // 
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            // 시간을 정수로 받아냄
+            int hour = DateTime.Now.Hour;
+            int minute = DateTime.Now.Minute;
+
+            // 지금시간이 오전인지, 오후인지를 출력하세요
+            if ( hour < 12) { Console.WriteLine("오전"); }
+            else { Console.WriteLine("오후"); }
+
+            // (여, 예비역) 12시~2시 사이인지를 체크해서 점심식사시간을 표시하세요
+            // (현역 남) 11:30 ~ 1:20 사이로 체크하기
+            // 여, 예비
+            if ((12 <= hour) && (hour < 14)) { Console.WriteLine("점심 먹으러 갑시다"); }
+            else { Console.WriteLine("점심시간이 아닙니다"); }
+
+            // 현역남
+            /*
+            switch(hour)
+            {
+                case 11:
+                    if (30 <= minute) { Console.WriteLine("점심먹으러 갑시다"); }
+                    else { Console.WriteLine("점심시간이 아닙니다"); }
+                    break;
+                case 12:
+                    Console.WriteLine("점심먹으러 갑시다");
+                    break;
+                case 13:
+                    if (20 >= minute) { Console.WriteLine("점심먹으러 갑시다"); }
+                    else { Console.WriteLine("점심시간이 아닙니다"); }
+                    break;
+                default:
+                    Console.WriteLine("점심시간이 아닙니다");
+                    break;
+            }
+            // 현역 남학생은 위의 switch문을 중첩if문으로 바꾸세요
+            */
+
+            hour = 13; minute = 25;
+            if (hour == 12) { Console.WriteLine("남:점심먹으러 갑시다"); }
+            else {
+                if (hour == 11 && 30 <= minute) { Console.WriteLine("남:점심먹으러 갑시다"); }
+                else if (hour == 13 && 20 >= minute) { Console.WriteLine("남:점심먹으러 갑시다"); }
+                else { Console.WriteLine("남:점심시간이 아닙니다"); }
+            }
+
+        }
+    }
+}
---
+++ InClass2023/0922.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 정수(0~100)를 입력받아서, 학점(A,B,C,D,F) 출력
+            int score;
+            string credit;  // ABCDF -> char
+            string inStr;
+
+            Console.Write("점수를 입력하세요 ");
+            // 키보드로부터 입력받음 (문자열)
+            inStr = Console.ReadLine();
+            // 문자열을 정수로 변환 
+            score = int.Parse(inStr);
+            // 점수로 학점 판단
+            if (score >= 90) { credit = "A"; }
+            else if ((80 <= score) && (score < 90)) { credit = "B"; }
+            else if ((70 <= score) && (score < 80)) { credit = "C"; }
+            else if ((60 <= score) && (score < 70)) { credit = "D"; }
+            else { credit = "F"; }
+
+            // A B C D F (현재)
+            // A+ A B+ B C+ C D+ D F (이렇게 나오도록 수정해 보세요) 각자
+
+            Console.WriteLine("입력한 " + score + "는 " + credit + "입니다");
+
+
+            // 정수 입력, 홀수? 짝수?? 판별해서 출력
+            Console.Write("(홀/짝 구분하는) 정수를 입력하세요 ");
+            // 입력받을 문자열, 변환하는 정수 -- 변수 선언
+            // 따로 변수선언X -- 앞에서 선언한 변수를 그대로 사용
+            inStr = Console.ReadLine();
+            score = int.Parse(inStr);
+
+            switch (score % 2)
+            {
+                case 0:
+                    Console.WriteLine("입력받은 수는 짝수");
+                    break;
+                case 1:
+                    Console.WriteLine("입력받은 수는 홀수");
+                    break;
+                default:
+                    Console.WriteLine("짝수/홀수 아닙니다");
+                    break;
+            }
+            // (앞에서 작성한) 점수 -- 학점 출력하는 부분 (if else if로 작성됨)
+            // 이놈을 switch문으로 바꿔 보세요
+
+            // (앞에서 작성한) 정수 -- 홀/짝? 출력
+            // 교재에서
+            Console.WriteLine((score % 2 == 0) ? "짝" : "홀");
+            
+            credit = ((score % 2) == 0) ? "짝수" : "홀수";
+            // 이놈을 if else문으로 바꿔 보세요 -- 각자
+            Console.WriteLine(credit);
+
+        }
+    }
+}
