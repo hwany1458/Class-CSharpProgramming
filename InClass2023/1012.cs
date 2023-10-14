9f8366149b1ad6558cae416af6b1f37cd4fdf8c0
hwany1458 <hwany1458@empal.com>
2023-10-15 05:01:05 +0900

    Add files via upload

---
+++ InClass2023/1012.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class2
+    {
+        static void Main1111(string[] args)
+        {
+            int[] distinctLook = { 7, 5, 4, 17, 8, 44 };
+
+            //for (초기식; 조건식; 종결식) {}
+
+            int cnt = 0;  // 초기식
+            while (cnt < distinctLook.Length)   // 조건식
+            {
+                Console.WriteLine(distinctLook[cnt]);
+                cnt++;  // 종결식
+            }
+
+            // 앞의 while() 반복문을 for()문으로 변경해 보세요
+            for(int idx=0; idx < distinctLook.Length; idx++)
+            {
+                Console.WriteLine(distinctLook[idx]);
+            }
+
+            Console.WriteLine("-------------------");
+            // 키보드부터 정수를 입력하고 (해당숫자를) 출력하는 코드를 작성하세요
+            // 입력받은 정수가 0이 아니면 계속 반복
+            // 0이면 반복을 빠져나옴
+
+            // (1) while반복문 사용
+            int inputNum = 0;   // 입력받은 정수 
+            // (초기값을 임의의 정수로 넣어줌)
+
+            // 초기값이 만약 0으로 지정했다면 어떻게 될까요?
+            // 초기값에 따라서, 조건문이 false가 되면, 
+            // 반복명령을 한번도 실행하지 않고 통과해 버림
+            
+            string inputStr;  // 입력받은 문자열
+
+            // while --> do while으로 변경
+            do
+            {
+                // 다음 입력을 받기 위해
+                Console.Write("[가위1, 바위2, 보3, 종료0] 숫자를 입력하세요 :");
+                inputStr = Console.ReadLine();
+                inputNum = int.Parse(inputStr);
+                // 가위바위보 게임을 완성시키려면, 여기서 컴퓨터입력을 처리
+                Console.WriteLine("입력한 값은 " + inputNum);
+            } while (inputNum != 0);
+
+            /* 
+            while(true)
+            {
+                // 반복하는 명령에서 조건을 거짓으로 만드는 것이 없을 경우, 무한반복
+                Console.WriteLine("사랑해");
+            }
+            */
+        }
+    }
+}
---
+++ InClass2023/1013.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class3
+    {
+        static void Main(string[] args)
+        {
+            // 키보드로부터 정수값(inputNum)을 하나 입력받아서,
+            // 1~inputNum 까지 반복하면서 숫자 출력
+            // (수정1) 위 내용을 수정해 보세요 ... (홀수만 출력되도록)
+            //    (A1) 반복문 안에 조건식을 넘는다 
+            //         (홀수인지 판별해서 맞을 경우 출력, 안맞으면 안 출력)
+
+            // (0) 변수선언
+            // (1) 입력받기
+            // (2) 조건체크해서 (출력을) 반복 
+
+            // (0) 2개 정도
+            string str;
+            int inputNum;
+
+            // (1)
+            Console.Write("정수를 입력하세요 ");  // 선택적으로
+            str = Console.ReadLine();
+            inputNum = int.Parse(str);
+
+            // (2) for(초기식; 조건식; 종결식)
+            for(int i = 1; i <= inputNum; i++)
+            {
+                // 전부 출력
+                //Console.WriteLine(i);
+
+                // (A1)
+                //if ((i%2) == 1)  // 홀수만 체크해서 출력
+                if ((i % 2) != 1)  // 짝수만 체크 (= 홀수가 아닌지를 체크)
+                {
+                    // 확인하기 위해 출력해 본거고 ..
+                    //Console.Write("[짝수] 안출력");
+                    //continue;
+                    // continue문을 만나면 현재 반복하고 있는 부분을 빠져나오고
+                    // 다음 반복으로 넘어감
+
+                    // (수정2) continue문을 break문으로 바꿔서 돌려봄
+                    break;
+                    // break문을 만나면 현재 반복하고 있는 부분을 빠져나오고
+                    // 더 이상 반복을 하지 않음
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
