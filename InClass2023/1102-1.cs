07a311f9b0b945f2e8e8dc776ab3884b9e0d1b96
hwany1458 <hwany1458@empal.com>
2023-11-03 05:21:17 +0900

    Add files via upload

---
+++ InClass2023/1102-1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            // 정수형 배열
+            int[] intArray = { 1,2,3,4,5 };
+            intArray[2] = 100;
+
+            // 클래스에서 객체를 인스턴스화
+            // 클래스 객체명 = new 클래스();
+            //Random obj = new Random();
+            List<int> obj1 = new List<int>();
+            List<string> obj2 = new List<string>();
+
+            obj1.Add(1);
+            obj1.Add(2);
+
+            for (int i=1; i<=10; i++)
+            {
+                obj1.Add(i*10);
+            }
+
+            obj1.Remove(10);
+
+            // 들어간 있는 놈(int)들을 출력
+            /*
+            for (int j=0; j<obj1.Count; j++) {
+                Console.WriteLine(obj1[j]);
+            }
+            */
+
+            // 숙제
+            // Add(50)
+            // Remove(50)
+
+            foreach(int item in obj1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 돌려보기 ..
+        }
+    }
+}
---
+++ InClass2023/1102.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program  // Program 이라는 이름으로 클래스를 정의
+    {
+        // 속성
+
+
+        // 기능
+        static void Main(string[] args)
+        {
+            // 가위바위보 (1판)
+            // (1) 사용자(가위1, 바위2, 보3) -- 키보드로부터
+            // (2) 컴퓨터(..) -- 랜덤하게 숫자를 뽑음 (Random 클래스 사용)
+            // (3) 사용자낸수 vs. 컴퓨터낸수 -- 비교해서 승무패를 판정
+            // (4) 결과 출력
+
+            // 승무패 계산
+            int win = 0;
+            int draw = 0;
+            int lose = 0;
+
+            Console.Write("가위바위보를 입력하세요 (가위:1, 바위:2, 보:3) ");
+            // (1)
+            string inputStr = Console.ReadLine();
+            int userInput = int.Parse(inputStr);
+
+            // (2)
+            // 클래스에서 객체를 인스턴스화
+            // 클래스 객체명 = new 클래스();
+            Random obj = new Random();
+            int comInput = obj.Next(1, 4);
+            Console.WriteLine("컴퓨터 램던수 =" + comInput);
+
+            // (3)
+            if (userInput == 1)  // 사용자 = 1 (가위)
+            {
+                if (comInput == 1) // 컴 = 1 (가위)
+                { Console.WriteLine("무승부"); draw++; }
+                else if (comInput == 2)  // 컴 = 2 (바위)
+                { Console.WriteLine("패"); lose++; }
+                else  // 컴 = 3 (보)
+                { Console.WriteLine("승"); win++; }
+            } else if (userInput == 2) // 사용자 = 2 (바위)
+            {
+                if (comInput == 1) // 컴 = 1 (가위)
+                { Console.WriteLine("승"); win++; }
+                else if (comInput == 2)  // 컴 = 2 (바위)
+                { Console.WriteLine("무승부"); draw++; }
+                else  // 컴 = 3 (보)
+                { Console.WriteLine("패"); lose++; }
+            }
+            else if (userInput == 3) // 사용자 = 3 (보)
+            {
+                if (comInput == 1) // 컴 = 1 (가위)
+                { Console.WriteLine("패"); lose++; }
+                else if (comInput == 2)  // 컴 = 2 (바위)
+                { Console.WriteLine("승"); win++; }
+                else  // 컴 = 3 (보)
+                { Console.WriteLine("무승부"); draw++; }
+            }
+            else { Console.WriteLine("잘못 입력했습니다");  }
+
+            Console.WriteLine(win + "승 " + draw + "무 " + lose + "패");
+
+            // 숙제
+            // do while() 을 사용해서 가위바위보 게임을 계속하게 수정
+        }  
+    }
+}
