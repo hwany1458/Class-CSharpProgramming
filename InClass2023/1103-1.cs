e03f078f317bdd43a5f993329dfdbfccb7db4024
hwany1458 <hwany1458@empal.com>
2023-11-04 21:28:52 +0900

    Add files via upload

---
+++ InClass2023/1103-1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        static void Main2(string[] args)
+        {
+            // 정수형 배열
+            int[] intArray = { 1,2,3,4,5 };
+            intArray[2] = 100;
+
+            // 클래스에서 객체를 인스턴스화
+            // 클래스 객체명 = new 클래스();
+            //Random obj = new Random();
+            List<int> obj1 = new List<int>();
+            List<string> obj2 = new List<string>();
+
+            obj1.Add(1);
+            obj1.Add(2);
+
+            for (int i=1; i<=10; i++)
+            {
+                obj1.Add(i*10);
+            }
+
+            obj1.Remove(10);
+
+           // 숙제
+            obj1.Add(50);
+            obj1.Remove(50);
+
+            int cnt = obj1.Count;
+            // 들어간 있는 놈(int)들을 출력
+            for (int j=0; j<obj1.Count; j++) {
+                Console.WriteLine("Count:" + (j+1) + "/" + cnt + ", Item:" + obj1[j]);
+            }
+            /*
+             * foreach(int item in obj1)
+            { 
+                Console.WriteLine(item);
+            }
+            */
+
+            // 돌려보기 ..
+            // 동일한 데이터(값50)가 2개 들어있을때
+            //(1) 앞의 데이터가 삭제
+            //(2) 뒤의 데이터가 삭제
+            //(3) 둘다 삭제
+            //(4) 둘다 안 삭제 (어떤 것을 삭제해야 하는지 모호하기 때문에)
+
+            int foundInt = obj1.Find(x => x == 1000);
+            Console.WriteLine(foundInt);
+        }
+    }
+}
---
+++ InClass2023/1103.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        {
+            // 클래스 --> 객체 (인스턴스화)
+            // 클래스 객체명 = new 클래스();
+            //Random obj1 = new Random();
+            //List<int> obj2 = new List<int>();
+
+            Console.WriteLine(Math.Abs(-10));
+            Console.WriteLine(Math.Min(10, 20));
+            Console.WriteLine(Math.Max(10, 20));
+
+            // 문제
+            // 키보드로부터 5개 정수를 입력받아, 
+            // 최대값과 최소값을 출력하세요
+            // (1)키보드로부터 입력을 받음 (5개) -- 저장
+            // 변수, 배열, 리스트
+            // (2)(배열이나 리스트에서) 하나씩 가져와서
+            // 현재 최소값, 최대값과 비교해서 받아줌(?)
+
+            int[] arr = { 0, 0, 0, 0, 0 };
+            string inputStr;
+            //int inputNum,
+            int minValue =100000000, maxValue=-100000000;
+
+            for (int i=0; i<5; i++)
+            { 
+                Console.Write("정수를 입력하세요 ");
+                inputStr = Console.ReadLine();
+                /* (절차를 순서대로 애기하느라..)
+                inputNum = int.Parse(inputStr);
+                arr[i] = inputNum;
+                */
+                arr[i] = int.Parse(inputStr);
+            }
+
+            for(int j=0; j<5; j++)
+            {
+                // 어느 놈이 작은지를 판별 (최소값 뽑을려고)
+                //if (arr[j] < minValue) { minValue = arr[j]; }
+                minValue = Math.Min(arr[j], minValue);
+
+                // 어느 놈이 큰지를 판별 (최대값 뽑을려고)
+                //if (arr[j] > maxValue) { maxValue = arr[j]; }
+                maxValue = Math.Max(arr[j], maxValue);
+            }
+
+            Console.WriteLine("최대값:" + maxValue + ", 최소값:" + minValue);
+        }
+    }
+}
